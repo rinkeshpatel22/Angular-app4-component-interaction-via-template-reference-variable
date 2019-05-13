473c23681a1511e63972f90fdd8b776c337d5a01
rinkeshpatel22 <rinkeshpatel22@gmail.com>
2019-05-13 22:27:38 +0530

    Initial commit

---
+++ src/app/demo/demo-parent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-parent',
+  templateUrl: './demo-parent.component.html',
+  styleUrls: ['./demo-parent.component.css']
+})
+export class DemoParentComponent {
+
+}
